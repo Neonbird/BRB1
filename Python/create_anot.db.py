dd6ae66c48d11ab010d744166c21125bb45e1a1c
Tanya Maslikova <kosha44@yandex.ru>
2019-10-24 00:47:57 +0300

    добавила sys.argv в create_anot.db.py

--- Python/create_anot.db.py
+++ Python/create_anot.db.py
@@ -1,6 +1,7 @@
 import gffutils
+import sys
 
 #создаем аннотацию один!! раз, она одна для всех фенотипов
-anot_db = gffutils.create_db("/home/neobird/BI/BRB1/gencode.v19.annotation.gtf_withproteinids",
+anot_db = gffutils.create_db(sys.argv[1],
                            dbfn='anot.db', force=True, keep_order=True,
                           disable_infer_genes=True, disable_infer_transcripts=True)
