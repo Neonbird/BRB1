c0e358a410f40580f7d3adff02f61e26c8492178
Tanya Maslikova <kosha44@yandex.ru>
2019-11-03 13:06:04 +0300

    Изменено описание

--- Python/gene_min_pval_test.py
+++ Python/gene_min_pval_test.py
@@ -31,38 +31,76 @@ df_snp = df_snp[['chr','pval', 'position']]
 #print(df_snp)
 #print(df_genes)
 
-dict_genes_snp = {}
+# dict_genes_snp = {}
+# for chr_type in ('1','2', '3', '4', '5', '6', '7', '8', '9', '10',
+#                  '11', '12', '13', '14', '15', '16', '17', '18',
+#                  '19', '20', '21', '22', 'M', 'X', 'Y'):
+#     # создаём датафреймы по генам и снипам только для одного типа хромосом
+#     df_snp_chr = df_snp.loc[df_snp.chr == chr_type]
+#     df_genes_chr = df_genes.loc[df_genes.chr == chr_type]
+#### ВАРИАНТ СЛОВАРЯ ПО ГЕНАМ
+#     #идём для каждого гена по строкам снипов с той же хромосомы, смотрим, входит ли снип в него
+#     for gene_row in df_genes_chr.itertuples(index=False):
+#         name_of_gene = gene_row[0]
+#         start_of_gene = int(gene_row[2])
+#         end_of_gene = int(gene_row[3])
+#         for snp_row in df_snp_chr.itertuples(index = False):
+#             pval_of_snp = snp_row[1]
+#             # проверка, не является ли пвал NaN
+#             if pval_of_snp == 'NaN':
+#                 break
+#             #     если явялется, берём сразу следующий снип
+#             else:
+#                 pval_of_snp = float(pval_of_snp)
+#             position_of_snp = int(snp_row[2])
+#             # проверяем, входит ли снип в этот ген
+#             if position_of_snp >= start_of_gene and position_of_snp <= end_of_gene:
+#                 # если ген ещё не в словаре то, присваимаем ему этот пвал
+#                 # если ген в словаре, если данный пвал меньше чем тот, что записан в словаре,
+#                 # меняем его на этот меньший пвал
+#                 if name_of_gene not in dict_genes_snp or pval_of_snp < dict_genes_snp[name_of_gene]:
+#                         dict_genes_snp[name_of_gene] = pval_of_snp
+# #print(dict_genes_snp)
+
+####ВАРИАНТ СЛОВАРЯ ПО СНП
+dict_snp_genes = {}
 for chr_type in ('1','2', '3', '4', '5', '6', '7', '8', '9', '10',
                  '11', '12', '13', '14', '15', '16', '17', '18',
                  '19', '20', '21', '22', 'M', 'X', 'Y'):
     # создаём датафреймы по генам и снипам только для одного типа хромосом
     df_snp_chr = df_snp.loc[df_snp.chr == chr_type]
     df_genes_chr = df_genes.loc[df_genes.chr == chr_type]
-    #идём для каждого гена по строкам снипов с той же хромосомы, смотрим, входит ли снип в него
-    for gene_row in df_genes_chr.itertuples(index=False):
-        name_of_gene = gene_row[0]
-        start_of_gene = int(gene_row[2])
-        end_of_gene = int(gene_row[3])
-        for snp_row in df_snp_chr.itertuples(index = False):
-            pval_of_snp = snp_row[1]
-            # проверка, не является ли пвал NaN
-            if pval_of_snp == 'NaN':
-                break
-            #     если явялется, берём сразу следующий снип
-            else:
-                pval_of_snp = float(pval_of_snp)
+    for snp_row in df_snp_chr.itertuples(index=False):
+        pval_of_snp = snp_row[1]
+        # проверка, не является ли пвал NaN
+        # если явялется, берём сразу следующий снип
+        if pval_of_snp == 'NaN':
+            break
+        else:
+            pval_of_snp = float(pval_of_snp)
             position_of_snp = int(snp_row[2])
-            # проверяем, входит ли снип в этот ген
+        for gene_row in df_genes_chr.itertuples(index=False):
+            name_of_gene = gene_row[0]
+            start_of_gene = int(gene_row[2])
+            end_of_gene = int(gene_row[3])
             if position_of_snp >= start_of_gene and position_of_snp <= end_of_gene:
-                # если ген ещё не в словаре то, присваимаем ему этот пвал
-                # если ген в словаре, если данный пвал меньше чем тот, что записан в словаре,
-                # меняем его на этот меньший пвал
-                if name_of_gene not in dict_genes_snp or pval_of_snp < dict_genes_snp[name_of_gene]:
-                        dict_genes_snp[name_of_gene] = pval_of_snp
-#print(dict_genes_snp)
+                dict_snp_genes[pval_of_snp] = name_of_gene
+
+dict_gene_minpval = {}
+for pval in dict_snp_genes.keys():
+    gene_name = dict_snp_genes[pval]
+    if gene_name not in dict_gene_minpval or pval < dict_gene_minpval[gene_name]:
+        dict_gene_minpval[gene_name] = pval
+
 
 import csv
+
 with open('gene_pval_100001_irnt.gwas.imputed_v3.both_sexes.csv', 'w') as csv_file:
     writer = csv.writer(csv_file)
-    for key, value in dict_genes_snp.items():
+    for key, value in dict_gene_minpval.items():
+       writer.writerow([key, value])
+
+with open('snp_gene.csv', 'w') as csv_file:
+    writer = csv.writer(csv_file)
+    for key, value in dict_snp_genes.items():
        writer.writerow([key, value])
